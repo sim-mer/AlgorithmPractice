7e3900937eacccd8f2e309f883ac832ff6570a64
Gyumin Sim <gyu2781@gmail.com>
2023-12-29 19:37:04 +0900

    2022 KAKAO BLIND RECRUITMENT - k진수에서 소수 개수 구하기

---
+++ src/PrimeCountInBaseK.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class PrimeCountInBaseK {
+    public static void main(String[] args) {
+        int n = 110011;
+        int k = 10;
+
+        Solution sol = new Solution();
+
+        System.out.println(sol.solution(n, k));
+    }
+
+    static class Solution {
+        public int solution(int n, int k) {
+            int answer = 0;
+
+            String str = Long.toString(n, k);
+            String[] arr = str.split("0");
+
+            for(String s : arr) {
+                if(s.isEmpty()) continue;
+                if(isPrime(Long.parseLong(s))) answer++;
+            }
+
+            return answer;
+        }
+
+        private boolean isPrime(long n) {
+            if(n == 1) return false;
+            for(int i = 2; i <= Math.sqrt(n); i++) {
+                if(n % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
