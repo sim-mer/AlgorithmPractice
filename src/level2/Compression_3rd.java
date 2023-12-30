5c7e5f63dbdf68e4f05eeb6f3b5f572a204b3e39
Gyumin Sim <gyu2781@gmail.com>
2023-12-30 22:49:17 +0900

    2018 KAKAO BLIND RECRUITMENT - [3차] 압축

---
+++ src/level2/Compression_3rd.java
@@ -0,0 +1,49 @@
+package level2;
+
+import java.util.*;
+
+public class Compression_3rd {
+    public static void main(String[] args) {
+        String msg = "KAKAO";
+
+        Solution sol = new Solution();
+
+        System.out.println(Arrays.toString(sol.solution(msg)));
+    }
+
+    static class Solution {
+        public int[] solution(String msg) {
+            Map<String, Integer> map = new HashMap<>();
+            List<Integer> list = new ArrayList<>();
+
+            for(int i = 0; i < 26; i++) {
+                map.put((char)('A' + i) + "", i + 1);
+            }
+
+            int idx = 27;
+
+            for(int i = 0; i < msg.length(); i++) {
+                String temp = msg.charAt(i) + "";
+                while(map.containsKey(temp)) {
+                    if (i == msg.length() - 1) {
+                        i++;
+                        break;
+                    }
+                    temp += msg.charAt(++i);
+                }
+                i--;
+
+                if(i == msg.length() - 1) {
+                    list.add(map.get(temp));
+                    break;
+                }
+
+                map.put(temp, idx++);
+                list.add(map.get(temp.substring(0, temp.length() - 1)));
+            }
+
+
+            return list.stream().mapToInt(Integer::intValue).toArray();
+        }
+    }
+}
