a50b624c2fc5a0034bcb704b8afc7ad6251103db
Sim, Gyu Min <gyu2781@knu.ac.kr>
2024-01-02 22:50:05 +0900

    [level 2] Title: [3차] 압축, Time: 22.37 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String msg) {
+        Map<String, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < 26; i++) {
+            map.put((char)('A' + i) + "", i + 1);
+        }
+
+        int idx = 27;
+
+        for(int i = 0; i < msg.length(); i++) {
+            String temp = msg.charAt(i) + "";
+            while(map.containsKey(temp)) {
+                    i++;
+                    if (i == msg.length()) break;
+                    temp += msg.charAt(i);
+            }
+            i--;
+
+            if(i == msg.length() - 1) {
+                list.add(map.get(temp));
+                break;
+            }
+
+            map.put(temp, idx++);
+            list.add(map.get(temp.substring(0, temp.length() - 1)));
+        }
+
+
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
