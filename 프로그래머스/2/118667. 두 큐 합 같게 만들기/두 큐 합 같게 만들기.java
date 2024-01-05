730302382ae22b0b5329df26b6b8c510075576f6
Sim, Gyu Min <gyu2781@knu.ac.kr>
2024-01-06 08:21:05 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 20.85 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,50 +1,49 @@
 import java.util.*;
+
 class Solution {
-        public int solution(int[] queue1, int[] queue2) {
-            int answer = 0;
+    public int solution(int[] queue1, int[] queue2) {
+        int answer = 0;
 
-            if(queue1.length == 0 && queue2.length == 0) return 0;
+        if(queue1.length == 0 && queue2.length == 0) return 0;
 
-            Deque<Integer> deque1 = new ArrayDeque<>();
-            Deque<Integer> deque2 = new ArrayDeque<>();
+        Deque<Integer> deque1 = new ArrayDeque<>();
+        Deque<Integer> deque2 = new ArrayDeque<>();
 
-            long sum = 0;
-            int max = 0;
+        long sum1 = 0;
+        long sum2 = 0;
+        int max = 0;
 
-            for(int i : queue1) {
-                deque1.add(i);
-                sum += i;
-                max = Math.max(max, i);
-            }
-            for(int i : queue2) {
-                deque2.add(i);
-                sum += i;
-                max = Math.max(max, i);
-            }
-
-            long goal = sum / 2;
-            if(max > goal) return -1;
-
-            long sum1 = deque1.stream().mapToLong(i -> i).sum();
-            long sum2 = deque2.stream().mapToLong(i -> i).sum();
-
-            while(true) {
-                if(answer > queue1.length + queue2.length + 2) return -1;
-                if(sum1 == sum2) break;
-
-                if(sum1 > sum2) {
-                    deque2.add(deque1.pollFirst());
-                    sum1 -= deque2.peekLast();
-                    sum2 += deque2.peekLast();
-                    answer++;
-                    continue;
-                }
-                deque1.add(deque2.pollFirst());
-                sum1 += deque1.peekLast();
-                sum2 -= deque1.peekLast();
-                answer++;
-            }
-
-            return answer;
+        for(int i : queue1) {
+            deque1.add(i);
+            sum1 += i;
+            max = Math.max(max, i);
         }
+        for(int i : queue2) {
+            deque2.add(i);
+            sum2 += i;
+            max = Math.max(max, i);
+        }
+
+        long goal = (sum1 + sum2) / 2;
+        if(max > goal) return -1;
+
+        while(true) {
+            if(answer > queue1.length + queue2.length + 2) return -1;
+            if(sum1 == sum2) break;
+
+            if(sum1 > sum2) {
+                deque2.add(deque1.pollFirst());
+                sum1 -= deque2.peekLast();
+                sum2 += deque2.peekLast();
+                answer++;
+                continue;
+            }
+            deque1.add(deque2.pollFirst());
+            sum1 += deque1.peekLast();
+            sum2 -= deque1.peekLast();
+            answer++;
+        }
+
+        return answer;
     }
+}
