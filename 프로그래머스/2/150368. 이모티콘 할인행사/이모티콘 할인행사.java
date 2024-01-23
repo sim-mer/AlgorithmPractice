34289554ae0f750512801aac9af1833d8a3e2a09
Sim, Gyu Min <gyu2781@knu.ac.kr>
2024-01-23 09:27:28 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 0.46 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -0,0 +1,61 @@
+import java.util.*;
+class Solution {
+    int[] answer = new int[2];
+    int[][] users;
+    int[] emoticons;
+    double[] discount = {0.9, 0.8, 0.7, 0.6};
+    public int[] solution(int[][] users, int[] emoticons) {
+        this.users = users;
+        this.emoticons = emoticons;
+
+        permutation(new ArrayList<>(), 0);
+
+        return answer;
+    }
+
+    private void permutation(List<Emoticon> list, int idx) {
+        if(list.size() == emoticons.length) {
+            int subscribe = 0;
+            double sum = 0;
+            for(int[] user : users) {
+                double userDiscount = 1 - ((double) user[0] * 0.01);
+                double tempSum = 0;
+                for(Emoticon emoticon : list) {
+                    if(emoticon.discount > userDiscount) continue;
+                    tempSum += emoticon.price;
+                }
+
+                if(tempSum >= user[1]) {
+                    subscribe++;
+                    continue;
+                }
+                sum += tempSum;
+            }
+
+            if(answer[0] < subscribe) {
+                answer[0] = subscribe;
+                answer[1] = (int) sum;
+                return;
+            }
+            if(answer[0] == subscribe && answer[1] < sum) {
+                answer[1] = (int) sum;
+            }
+            return;
+        }
+
+        for(double d : discount) {
+            list.add(new Emoticon(emoticons[idx] * d, d));
+            permutation(list, idx + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+
+    class Emoticon {
+        double price;
+        double discount;
+        Emoticon(double price, double discount) {
+            this.price = price;
+            this.discount = discount;
+        }
+    }
+}
