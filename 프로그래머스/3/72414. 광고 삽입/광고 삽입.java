788684f8144dc6f77ac09f6a2b8e2eee57717b2b
Sim, Gyu Min <gyu2781@knu.ac.kr>
2024-01-26 16:17:55 +0900

    [level 3] Title: 광고 삽입, Time: 6.08 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String solution(String play_time, String adv_time, String[] logs) {
+        if(play_time.equals(adv_time)) return "00:00:00";
+
+        int playTime = strToSec(play_time);
+        int advTime = strToSec(adv_time);
+        int[] time = new int[playTime + 1];
+
+        for(String log : logs) {
+            String[] arr = log.split("-");
+            int start = strToSec(arr[0]);
+            int end = strToSec(arr[1]);
+
+            for(int i = start; i < end; i++) time[i]++;
+        }
+
+        long sum = 0, max = 0;
+        for(int i = 0;i < advTime; i++) sum += time[i];
+        max = sum;
+        int insertTime = 0;
+
+        for(int i = advTime; i < playTime; i++) {
+            sum = sum + time[i] - time[i - advTime];
+            if(max < sum) {
+                max = sum;
+                insertTime = i - advTime + 1;
+            }
+        }
+
+        int hour = insertTime / 3600;
+        int min = (insertTime % 3600) / 60;
+        int sec = insertTime % 60;
+
+        return String.format("%02d:%02d:%02d", hour, min, sec);
+    }
+
+    private int strToSec(String str) {
+        String[] time = str.split(":");
+        return Integer.parseInt(time[0]) * 3600 + Integer.parseInt(time[1]) * 60 + Integer.parseInt(time[2]);
+    }
+}
