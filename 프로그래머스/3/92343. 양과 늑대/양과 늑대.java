0507dbf95287e2c33868f19cb3c669d92cb91735
Sim, Gyu Min <gyu2781@knu.ac.kr>
2024-01-27 22:54:00 +0900

    [level 3] Title: 양과 늑대, Time: 0.35 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -3,24 +3,22 @@ class Solution {
     int answer = 0;
     int[][] edges;
     Node[] nodes;
+    boolean[] visited;
 
     public int solution(int[] info, int[][] edges) {
         nodes = new Node[info.length];
         this.edges = edges;
+        visited = new boolean[info.length];
 
         for(int i = 0; i < info.length; i++) {
             nodes[i] = new Node(i, info[i]);
         }
 
-        for(int[] edge : edges) {
-            nodes[edge[0]].childList.add(nodes[edge[1]]);
-        }
-
-        a(nodes[0], 0, 0, new boolean[info.length]);
+        dfs(nodes[0], 0, 0);
         return answer;
     }
 
-    private void a (Node root, int sheep, int wolf, boolean[] visited) {
+    private void dfs(Node root, int sheep, int wolf) {
         if(root.info == 0) {
             sheep++;
             answer = Math.max(answer, sheep);
@@ -34,7 +32,7 @@ class Solution {
 
         for(int[] edge : edges) {
             if(visited[edge[0]] && !visited[edge[1]]) {
-                a(nodes[edge[1]], sheep, wolf, visited);
+                dfs(nodes[edge[1]], sheep, wolf);
                 visited[edge[1]] = false;
             }
         }
@@ -43,11 +41,9 @@ class Solution {
     class Node {
         int vertex;
         int info;
-        List<Node> childList;
         public Node(int vertex, int info) {
             this.vertex = vertex;
             this.info = info;
-            this.childList = new ArrayList<>();
         }
     }
 }
