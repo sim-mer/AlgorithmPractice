0e3e0fe4bd678937297bfeb0423a3e90ae1e4daa
Sim, Gyu Min <gyu2781@knu.ac.kr>
2024-02-01 01:34:28 +0900

    [level 2] Title: 순위 검색, Time: 1212.50 ms, Memory: 292 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.java
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.java
@@ -1,85 +1,55 @@
 import java.util.*; 
 class Solution {
-    public int[] solution(String[] info, String[] query) {
-        ArrayList<Integer>[][][][] applicants = new ArrayList[3][2][2][2];
+    static HashMap<String, List<Integer>> map;
+ 
+    public static int[] solution(String[] info, String[] query) {
         int[] answer = new int[query.length];
-
-        for(int i = 0; i < 3; i++) {
-            for(int j = 0; j < 2; j++) {
-                for(int k = 0; k < 2; k++) {
-                    for(int l = 0; l < 2; l++) {
-                        applicants[i][j][k][l] = new ArrayList<>();
-                    }
-                }
-            }
+        map = new HashMap<String, List<Integer>>();
+ 
+        for (int i = 0; i < info.length; i++) {
+            String[] p = info[i].split(" ");
+            makeSentence(p, "", 0);
         }
-
-        for(String applicant : info) {
-            String[] infoArr = applicant.split(" ");
-            int score = Integer.parseInt(infoArr[4]);
-            int lang = infoArr[0].equals("cpp") ? 0 : infoArr[0].equals("java") ? 1 : 2;
-            int job = infoArr[1].equals("backend") ? 0 : 1;
-            int career = infoArr[2].equals("junior") ? 0 : 1;
-            int food = infoArr[3].equals("chicken") ? 0 : 1;
-
-            applicants[lang][job][career][food].add(score);
+ 
+        for (String key : map.keySet())
+            Collections.sort(map.get(key));
+ 
+        for (int i = 0; i < query.length; i++) {
+            query[i] = query[i].replaceAll(" and ", "");
+            String[] q = query[i].split(" ");
+            answer[i] = map.containsKey(q[0]) ? binarySearch(q[0], Integer.parseInt(q[1])) : 0;
         }
-
-        for(int i = 0; i < 3; i++) {
-            for(int j = 0; j < 2; j++) {
-                for(int k = 0; k < 2; k++) {
-                    for(int l = 0; l < 2; l++) {
-                        applicants[i][j][k][l].sort(null);
-                    }
-                }
-            }
-        }
-
-        int idx = 0;
-        for(String q : query) {
-            String[] split = q.split(" and ");
-            String[] last = split[3].split(" ");
-            split[3] = last[0];
-            int score = Integer.parseInt(last[1]);
-
-            int lang = split[0].equals("cpp") ? 0 : split[0].equals("java") ? 1 : split[0].equals("python") ? 2 : -1;
-            int job = split[1].equals("backend") ? 0 : split[1].equals("frontend") ? 1 : -1;
-            int career = split[2].equals("junior") ? 0 : split[2].equals("senior") ? 1 : -1;
-            int food = split[3].equals("chicken") ? 0 : split[3].equals("pizza") ? 1 : -1;
-
-            int count = 0;
-            for(int i = 0; i < 3; i++) {
-                if(lang != -1 && lang != i) continue;
-                for(int j = 0; j < 2; j++) {
-                    if(job != -1 && job != j) continue;
-                    for(int k = 0; k < 2; k++) {
-                        if(career != -1 && career != k) continue;
-                        for(int l = 0; l < 2; l++) {
-                            if(food != -1 && food != l) continue;
-                            count += applicants[i][j][k][l].size() - binSearch(applicants[i][j][k][l], score);
-                        }
-                    }
-                }
-            }
-            answer[idx++] = count;
-        }
-
+ 
         return answer;
     }
-
-    private int binSearch(List<Integer> list, int score) {
-        int first = 0;
-        int last = list.size() - 1;
-        int mid = 0;
-
-        while(first <= last) {
-            mid = (first + last) / 2;
-            if(list.get(mid) < score) {
-                first = mid + 1;
-            } else {
-                last = mid - 1;
-            }
+ 
+    // 이분 탐색
+    private static int binarySearch(String key, int score) {
+        List<Integer> list = map.get(key);
+        int start = 0, end = list.size() - 1;
+ 
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (list.get(mid) < score)
+                start = mid + 1;
+            else
+                end = mid - 1;
         }
-        return first;
+ 
+        return list.size() - start;
+    }
+ 
+    // info가 포함될 수 있는 문장
+    private static void makeSentence(String[] p, String str, int cnt) {
+        if (cnt == 4) {
+            if (!map.containsKey(str)) {
+                List<Integer> list = new ArrayList<Integer>();
+                map.put(str, list);
+            }
+            map.get(str).add(Integer.parseInt(p[4]));
+            return;
+        }
+        makeSentence(p, str + "-", cnt + 1);
+        makeSentence(p, str + p[cnt], cnt + 1);
     }
 }
