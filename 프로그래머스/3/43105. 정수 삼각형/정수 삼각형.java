68313f13f3e8b7f43a8f989b2c568c7f0550bf08
Sim, Gyu Min <gyu2781@knu.ac.kr>
2024-08-07 02:33:56 +0900

    [level 3] Title: 정수 삼각형, Time: 8.51 ms, Memory: 59.2 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -7,19 +7,11 @@ class Solution {
         
         for(int i = 1; i < triangle.length; i++) {
             
-            for(int j = 0; j < triangle[i].length; j++) {
-                
-                if(j == 0) {
-                    triangle[i][j] += triangle[i - 1][j];
-                    continue;
-                }
-                
-                if(j == triangle[i].length - 1) {
-                    triangle[i][j] += triangle[i - 1][j - 1];
-                    continue;
-                }
-                
-                int pre = compare(triangle[i - 1][j], triangle[i - 1][j - 1]);
+            triangle[i][0] += triangle[i - 1][0];
+            triangle[i][i] += triangle[i - 1][i - 1];
+            
+            for(int j = 1; j < i; j++) {             
+                int pre = Math.max(triangle[i - 1][j], triangle[i - 1][j - 1]);
                 triangle[i][j] += pre;
                 
             }
@@ -33,11 +25,5 @@ class Solution {
         }
         
         return max;
-    }
-    
-    private int compare(int a, int b) {
-        if(a > b) return a;
-        if(b > a) return b;
-        return a;
     }
 }
