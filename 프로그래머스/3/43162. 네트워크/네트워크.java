0e09c5ecb2f3350e57aec4d7a9fea6bf21075ea4
Sim, Gyu Min <gyu2781@knu.ac.kr>
2024-08-27 09:07:28 +0900

    [level 3] Title: 네트워크, Time: 0.30 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -3,39 +3,27 @@ class Solution {
     boolean[] visited;
 
     public int solution(int n, int[][] computers) {
-        List<Integer>[] graph = new ArrayList[n];
         visited = new boolean[n];
-        
-        for (int i = 0; i < n; i++) {
-            graph[i] = new ArrayList<>();
-        }        
-        
-        for (int i = 0; i < computers.length; i++) {
-            for (int j = 0; j < n; j++) {
-                if(computers[i][j] == 1 && i != j) {
-                    graph[i].add(j);
-                }
-            }
-        }
-        
         
         int answer = 0;
         for(int i = 0; i < n; i++){
             if(visited[i]) continue;
-            dfs(i, graph);
+            dfs(i, computers);
             answer++;
         }
         return answer;
     }
     
-    private void dfs(Integer start, List<Integer>[] graph) {
+    private void dfs(int start, int[][] computers) {
         if(visited[start]) return;
         
         visited[start] = true;
         
-        for(Integer node : graph[start]) {
-            if(start == node) continue;
-            dfs(node, graph);
+        for(int i = 0; i < computers.length; i++) {
+            if(i == start) continue;
+            if(computers[start][i] == 1) {
+                dfs(i, computers);
+            }
         }
         
     }
