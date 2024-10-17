16ad87e82bc2dabb010eb0c0e223f4c14343928a
Sim, Gyu Min <gyu2781@knu.ac.kr>
2024-10-17 18:26:20 +0900

    [level 3] Title: 기지국 설치, Time: 0.59 ms, Memory: 52.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] stations, int w) {
+        int answer = 0;
+        
+        int idx = 1;
+        int range = w * 2 + 1;
+        
+        for(int station : stations) {
+            int start = station - w;
+            
+            if(idx < start) {
+                int empty = start - idx;
+                answer += empty / range;
+                if(empty % range != 0) {
+                    answer += 1;
+                }
+            }
+            idx = station + w + 1;
+        }
+        
+        if(idx <= n) {
+            int empty = n - idx + 1;
+            answer += empty / range;
+            if(empty % range != 0) {
+                answer += 1;
+            }
+        }
+
+        return answer;
+    }
+}
